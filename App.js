d4c7d147d9f2f2373de6711967591a3b5a739f42
Chester <89991461+ChesterJaz@users.noreply.github.com>
2023-08-30 00:37:34 +0800

    up

--- App.js
+++ App.js
@@ -2,15 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Background from './src/screens/Background'
 import { useState } from 'react'
-
 import Home from './src/screens/Home'
-import Login from './src/components/Login'
+
 
 const App = () => {
   return (
     <Background>
-    
-      <Home/>
+    <Home/>
+     
     </Background>
   )
 }
