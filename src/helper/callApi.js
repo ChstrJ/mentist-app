05216947767d39b76a802a9347f5c6e4066a69ba
Al Cedric <neongenesis531@yahoo.com.ph>
2023-09-08 23:52:04 +0800

    feature: Changes in CallApi/Axios Development

--- src/helper/callApi.js
+++ src/helper/callApi.js
@@ -2,10 +2,28 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import axios from 'axios';
 
+
+//Axios Header
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.headers.post['Accept'] = 'application/json'
+// Axios URL
 axios.defaults.baseURL = 'https://mentist.onrender.com/api/v1/'
 
+//Axios Token Interceptor
+axios.interceptors.request.use(
+    async config => {
+        const token = await AsyncStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = "Bearer " + token
+        }
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    }
+)
+
+//Axios function API
 export const callApi = async (method, url, data) => {
     return axios({
         method, url, data
