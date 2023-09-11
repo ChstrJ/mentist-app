088cd906d9b3f575259e7a2bbe88e37df2b121c7
Chester <89991461+ChesterJaz@users.noreply.github.com>
2023-09-12 01:23:02 +0800

    fixed

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -17,36 +17,29 @@ import {useNavigation} from '@react-navigation/native';
 import BackButton from '../components/BackButton';
 import styles from '../components/styles';
 import callApi from '../helper/callApi';
-import { err } from 'react-native-svg/lib/typescript/xml';
+import {err} from 'react-native-svg/lib/typescript/xml';
 
 const LogIn = () => {
   const navigation = useNavigation();
   const [hidePass, setHidePass] = useState(true);
-  
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+
+  const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
 
   const Data = {
-    e_mail: email, 
-    p_word: password
+    username: username,
+    password: password,
+  };
+
+  const logIn = async data => {
+      const response = await callApi('post', '/login', data)
+        .then(val =>val.status == 200 ? navigation.push('Dashboard'): navigation.push('LogIn'),
+        )
+        .catch(e => console.log(e.response.data));
+   
   }
 
-  const logIn = async(data) =>{
-    try {
-      const response = await callApi('post', '/login', data)
-      console.log(response.data)
-      
-      if (response.data.success){
-        navigation.navigate('Dashboard')
-      }
-      else{
-        Alert.alert('Login failed')
-      } 
-    } catch (error) {
-      console.error('APU request error', error)
-      Alert.alert('API request error', error)
-    }
-  }
   const togglePasswordVisibility = () => {
     setHidePass(!hidePass);
   };
@@ -73,15 +66,15 @@ const LogIn = () => {
           <Text className="text-black text-xl mt-10 mb-2">Login Account</Text>
 
           <TextInput
-            className="w-[350] mt-2 rounded-md"
+            className="w-[350] mt-5 rounded-lg"
+            label="Username"
             mode="outlined"
-            label="Email"
-            left={<TextInput.Icon icon={'email'} />}
             activeOutlineColor="green"
-            onChangeText={value => setEmail(value)}
+            left={<TextInput.Icon icon={'account'} />}
+            onChangeText={values => setUsername(values)}
           />
 
-            <TextInput
+          <TextInput
             className="w-[350] mt-2 rounded-md"
             mode="outlined"
             label="Password"
@@ -90,14 +83,12 @@ const LogIn = () => {
             left={<TextInput.Icon icon={'key'} />}
             right={
               <TextInput.Icon
-                icon={hidePass ? 'eye-off' : 'eye'} 
-                onPress={togglePasswordVisibility} 
+                icon={hidePass ? 'eye-off' : 'eye'}
+                onPress={togglePasswordVisibility}
               />
             }
             onChangeText={value => setPassword(value)}
           />
-
-          
 
           <View className="flex items-center justify-center">
             <Text className="text-black mt-5">
@@ -109,22 +100,18 @@ const LogIn = () => {
                     marginVertical: '20',
                   }}>
                   {' Signup here'}
-                  
                 </Text>
               </TouchableOpacity>
             </Text>
 
             <View className="flex justify-center items-center">
-                <TouchableOpacity
+              <TouchableOpacity
                 // onPress={() => navigation.navigate('Dashboard')}
-                  onPress={() => logIn(Data)}
-                  style={[
-                    styles.submitBtn,
-                    {backgroundColor: '#6FF484'},
-                  ]}>
-                  <Text style={styles.submitBtnTxt}>Signup</Text>
-                </TouchableOpacity>
-              </View>
+                onPress={() => logIn(Data)}
+                style={[styles.submitBtn, {backgroundColor: '#6FF484'}]}>
+                <Text style={styles.submitBtnTxt}>Signup</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Background>
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -35,20 +35,17 @@ const SignUp = () => {
   };
 
   const handleSubmit = async data => {
-    if (password === confirmPassword) {
-      setLoading(true);
-      
-      const response = await callApi('post', '/register', data)
-        .then(val =>
-          val.status == 200
-            ? navigation.push('LogIn')
-            : navigation.push('SignUp'), Alert.alert("Failed Registration. Please try again"))
-        .catch(e => console.log(e))
+    
+      if (password === confirmPassword) {
+        setLoading(true)
+        const response = await callApi('post', '/register', data)
+        .then(val =>val.status == 200 ? navigation.push('LogIn') : navigation.push('SignUp'))
+        .catch(e => console.log(e.response.data))
 
         .finally(() => {
           setTimeout(() => {
             setLoading(false);
-          }, 1000);
+          }, 800);
         });
     } else {
       Alert.alert("Password doesn't match");
@@ -63,126 +60,128 @@ const SignUp = () => {
   };
 
   return (
+    
     <ScrollView
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{flexGrow: 1}}>
       {isLoading ? <Loader/> : 
-        <Background>
-          <BackButton goBack={navigation.goBack} />
-          <View style={styles.CenterContainer}>
-            <Image
-              className="mt-20"
-              source={require('../assets/logo.png')}
-              style={{
-                width: 250,
-                height: 130,
-              }}
-            />
-          </View>
+      <Background>
+        <BackButton goBack={navigation.goBack} />
+        <View style={styles.CenterContainer}>
+        
+          <Image
+            className="mt-20"
+            source={require('../assets/logo.png')}
+            style={{
+              width: 250,
+              height: 130,
+            }}
+          />
+        </View>
+
+        <View className="flex justify-center items-center">
+          <Text className=" text-black text-xl mt-10">Create an account</Text>
+
+          <TextInput
+            className="w-[350] mt-5 rounded-lg"
+            label="First Name"
+            mode="outlined"
+            activeOutlineColor="green"
+            left={<TextInput.Icon icon={'account'} />}
+            onChangeText={value => setFirstName(value)}
+          />
+
+          <TextInput
+            className="w-[350] mt-5 rounded-lg"
+            label="Last Name"
+            mode="outlined"
+            activeOutlineColor="green"
+            left={<TextInput.Icon icon={'account'} />}
+            onChangeText={values => setlastName(values)}
+          />
+
+          <TextInput
+            className="w-[350] mt-5 rounded-lg"
+            label="Username"
+            mode="outlined"
+            activeOutlineColor="green"
+            left={<TextInput.Icon icon={'account'} />}
+            onChangeText={values => setUsername(values)}
+          />
+
+          <TextInput
+            className="w-[350] mt-5 rounded-lg"
+            label="Email"
+            mode="outlined"
+            activeOutlineColor="green"
+            left={<TextInput.Icon icon={'email'} />}
+            onChangeText={values => setEmail(values)}
+          />
+
+          <TextInput
+            className="w-[350] mt-5 rounded-lg"
+            label="Password"
+            mode="outlined"
+            activeOutlineColor="green"
+            secureTextEntry={hidePass}
+            left={<TextInput.Icon icon={'key'} />}
+            right={
+              <TextInput.Icon
+                icon={hidePass ? 'eye-off' : 'eye'}
+                onPress={togglePasswordVisibility}
+              />
+            }
+            onChangeText={value => setPassword(value)}
+          />
+
+          <TextInput
+            className="w-[350] mt-5 rounded-lg"
+            label="Confirm Password"
+            mode="outlined"
+            activeOutlineColor="green"
+            secureTextEntry={hidePass}
+            left={<TextInput.Icon icon={'key'} />}
+            right={
+              <TextInput.Icon
+                icon={hidePass ? 'eye-off' : 'eye'}
+                onPress={togglePasswordVisibility}
+              />
+            }
+            onChangeText={value => setConfirmPassword(value)}
+          />
 
           <View className="flex justify-center items-center">
-            <Text className=" text-black text-xl mt-10">Create an account</Text>
-
-            <TextInput
-              className="w-[350] mt-5 rounded-lg"
-              label="First Name"
-              mode="outlined"
-              activeOutlineColor="green"
-              left={<TextInput.Icon icon={'account'} />}
-              onChangeText={value => setFirstName(value)}
-            />
-
-            <TextInput
-              className="w-[350] mt-5 rounded-lg"
-              label="Last Name"
-              mode="outlined"
-              activeOutlineColor="green"
-              left={<TextInput.Icon icon={'account'} />}
-              onChangeText={values => setlastName(values)}
-            />
-
-            <TextInput
-              className="w-[350] mt-5 rounded-lg"
-              label="Username"
-              mode="outlined"
-              activeOutlineColor="green"
-              left={<TextInput.Icon icon={'account'} />}
-              onChangeText={values => setUsername(values)}
-            />
-
-            <TextInput
-              className="w-[350] mt-5 rounded-lg"
-              label="Email"
-              mode="outlined"
-              activeOutlineColor="green"
-              left={<TextInput.Icon icon={'email'} />}
-              onChangeText={values => setEmail(values)}
-            />
-
-            <TextInput
-              className="w-[350] mt-5 rounded-lg"
-              label="Password"
-              mode="outlined"
-              activeOutlineColor="green"
-              secureTextEntry={hidePass}
-              left={<TextInput.Icon icon={'key'} />}
-              right={
-                <TextInput.Icon
-                  icon={hidePass ? 'eye-off' : 'eye'}
-                  onPress={togglePasswordVisibility}
-                />
+            <TouchableOpacity
+              onPress={() => handleSubmit(Data)}
+              disabled={
+                !username ||
+                !firstName ||
+                !lastName ||
+                !email ||
+                !password ||
+                !confirmPassword
               }
-              onChangeText={value => setPassword(value)}
-            />
-
-            <TextInput
-              className="w-[350] mt-5 rounded-lg"
-              label="Confirm Password"
-              mode="outlined"
-              activeOutlineColor="green"
-              secureTextEntry={hidePass}
-              left={<TextInput.Icon icon={'key'} />}
-              right={
-                <TextInput.Icon
-                  icon={hidePass ? 'eye-off' : 'eye'}
-                  onPress={togglePasswordVisibility}
-                />
-              }
-              onChangeText={value => setConfirmPassword(value)}
-            />
-
-            <View className="flex justify-center items-center">
-            
-              <TouchableOpacity
-                onPress={() => handleSubmit(Data)}
-                disabled={
-                  !username ||
-                  !firstName ||
-                  !lastName ||
-                  !email ||
-                  !password ||
-                  !confirmPassword
-                }
-                style={[
-                  styles.submitBtn,
-                  {
-                    backgroundColor:
-                      !username ||
-                      !firstName ||
-                      !lastName ||
-                      !email ||
-                      !password ||
-                      !confirmPassword
-                        ? 'rgba(193, 240, 208, 0.2)'
-                        : '#6FF484',
-                  },
-                ]}>
-                <Text style={styles.submitBtnTxt}>Signup</Text>
-              </TouchableOpacity>
-            </View>
+              style={[
+                styles.submitBtn,
+                {
+                  backgroundColor:
+                    !username ||
+                    !firstName ||
+                    !lastName ||
+                    !email ||
+                    !password ||
+                    !confirmPassword
+                      ? 'rgba(0, 0, 0, 0.2)'
+                      : '#6FF484',
+                },
+              ]}>
+              <Text style={styles.submitBtnTxt}>Signup</Text>
+            </TouchableOpacity>
           </View>
-        </Background>
-      }
+        </View>
+        
+      </Background>
+    }
     </ScrollView>
   );
 };
