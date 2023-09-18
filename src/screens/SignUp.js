3124787b398baa4805e53b63f929e5d38383f350
Chester <89991461+ChesterJaz@users.noreply.github.com>
2023-09-18 15:59:45 +0800

    3

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -82,8 +82,7 @@ const SignUp = () => {
     setTimeout(() => {
       setLoading(false);
     }, 500);
-
-
+  };
 
   // for showing and hiding pass
   const [hidePass, setHidePass] = useState(true);
