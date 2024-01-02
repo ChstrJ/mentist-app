5312f0a5228ae6f7e86c4b2596913e2e49325655
Chester <89991461+ChesterJaz@users.noreply.github.com>
2024-01-02 12:08:10 +0800

    helpline

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,7 +1,4 @@
-import {
-  View,
-  ScrollView,
-} from 'react-native';
+import {View, ScrollView, TouchableOpacity, StyleSheet} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import Background from './Background';
 import {useNavigation} from '@react-navigation/native';
@@ -17,7 +14,7 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import {callApi} from '../helper/callApi';
-import { s } from 'react-native-size-matters';
+import {s} from 'react-native-size-matters';
 import LogoutBtn from '../components/Logout';
 import Btn from '../components/Btn';
 
@@ -42,12 +39,11 @@ const Dashboard = () => {
 
   const handleLogout = async () => {
     const removed = removeData();
-    removed ? (navigation.navigate('Home')) : console.log('Error logging out');
+    removed ? navigation.navigate('Home') : console.log('Error logging out');
   };
 
-
   useEffect(() => {
-    getUser()
+    getUser();
   });
 
   const getAppoint = () => {
@@ -87,51 +83,72 @@ const Dashboard = () => {
 
   return (
     <Background>
-     <ScrollView
-    showsVerticalScrollIndicator={false}
-    contentContainerStyle={{flexGrow: 1}}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{flexGrow: 1}}>
+        <LogoutBtn onPress={() => handleLogout()} />
 
-      <LogoutBtn
-        onPress={() => handleLogout()}
-      />
-    
-      <View style={{marginTop: hp(2)}} className="flex items-center">
-        <Logo />
-        <Text className="mt-5" style={styles.fontHomeSub}>
-          Good to see you here, {firstName}{' '}
-        </Text>
-      </View>
+        <View style={{marginTop: hp(2)}} className="flex items-center">
+          <Logo />
+          <Text className="mt-5" style={styles.fontHomeSub}>
+            Good to see you here, {firstName}{' '}
+          </Text>
+        </View>
 
+        <View className="flex items-center" style={{marginTop: s(5)}}>
+          <Action
+            actionLabel="Chatbot AI"
+            source={require('../assets/chatbot.png')}
+            Press={() => navigation.push('Chatscreen')}
+          />
+          <Action
+            actionLabel="Create Appointment"
+            source={require('../assets/appointment.png')}
+            Press={getAppoint}
+          />
+          <Action
+            actionLabel="My Progress"
+            source={require('../assets/development.png')}
+            Press={() => navigation.push('Progress')}
+          />
 
-      <View className="flex items-center" style={{marginTop: s(5)}}>
-        <Action
-          actionLabel="Chatbot AI"
-          source={require('../assets/chatbot.png')}
-          Press={() => navigation.push('Chatscreen')}
-        />
-        <Action
-          actionLabel="Create Appointment"
-          source={require('../assets/appointment.png')}
-          Press={getAppoint}
-        />
-        <Action
-          actionLabel="My Progress"
-          source={require('../assets/development.png')}
-          Press={() => navigation.push('Progress')}
-        />
-
-       <Btn
-        btnLabel={"Contact Helplines"}
-        onPress={() => navigation.push("Helplines")}
-       /> 
-
-        
-
-        
-
-      </View>
+          <View style={[styles1.container, styles1.bottomBar]}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                marginTop: 5,
+              }}>
+              <Text style={styles.fontText}>Need to call someone?</Text>
+              <TouchableOpacity onPress={() => navigation.push('Helplines')}>
+                <Text
+                  style={[{color: 'green', marginLeft: 5}, styles.fontText]}>
+                  Click here
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </ScrollView>
     </Background>
   );
 };
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bottomBar: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    padding: 10,
+    backgroundColor: 'lightgrey',
+  },
+  fontText: {
+    fontSize: 16,
+  },
+});
+
 export default Dashboard;
